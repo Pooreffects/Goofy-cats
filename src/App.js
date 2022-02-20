bfe9e48464861416d55f66f2e9f224138692bb14
Pooreffects <pooreffects@gmail.com>
2022-02-20 22:42:49 +0000

    feat: goofing around with the word goofy lol

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [cat, setCat] = useState(null);
+
+  const handleFetch = () => {
+    fetch('https://api.thecatapi.com/v1/images/search') // open source cat API with randomize query
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setCat(data);
+      });
+  };
+
+  useEffect(() => {
+    handleFetch();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <h2>Melancholy search for another cat 😁</h2>
       </header>
+
+      <div className="cat-container">
+        {cat &&
+          cat.map((kitty) => (
+            <img src={kitty.url} key={kitty.id} alt="random-cat" />
+          ))}
+      </div>
+      <button onClick={handleFetch} className="btn">
+        Another one!
+      </button>
     </div>
   );
 }
